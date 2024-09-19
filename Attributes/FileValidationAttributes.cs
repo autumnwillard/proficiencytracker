0a11f5a5c62acd488443d04db93c27a465b1aa5e
Autumn Willard <autumn@hey.com>
2024-09-18 22:09:56 -0500

    File validation attribute

---
+++ Attributes/FileValidationAttributes.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace ProficiencyTracker.Attributes
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int _maxFileSize;
+        public MaxFileSizeAttribute(int maxFileSize)
+        {
+            _maxFileSize = maxFileSize;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                if (file.Length > _maxFileSize)
+                {
+                    return new ValidationResult($"File size cannot exceed {_maxFileSize} bytes");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (!_extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult($"File extension {extension} is not allowed");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,14 +1,19 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+using ProficiencyTracker.Attributes;
 
 namespace ProficiencyTracker.Models 
 {
     public class Student 
     {
-        public Student(string name, int userId, string? notes = null) 
+        public Student(string name, int userId, string? notes = null, string? photoPath = null) 
         {
             Name = name;
             UserId = userId;
             Notes = notes;
+            PhotoPath = photoPath;
+            Progress = new List<StudentProgress>();
         }
         public int Id { get; set; }
         [Required]
@@ -17,7 +22,13 @@ namespace ProficiencyTracker.Models
         public string? Notes { get; set; }
         public int UserId { get; set; }
         public User User { get; set; } = null!;
-        public List<StudentProgress> Progress { get; set; } = new List<StudentProgress>();
 
+        string? PhotoPath { get; set; }
+        [NotMapped]
+        [Display(Name = "Profile Picture")]
+        [MaxFileSize(5 * 1024 * 1024)] // 5 MB
+        [AllowedExtensions(new string[] { ".jpg", ".png", ".gif" })]
+        public IFormFile? PhotoFile { get; set; }
+        public List<StudentProgress> Progress { get; set; } = new List<StudentProgress>();
     }
 }
