d9f051d87bd7fd63125254e8a13399804617bdaf
Autumn Willard <autumn@hey.com>
2024-09-18 22:47:02 -0500

    Update AppDbContext for users

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -1,20 +1,24 @@
 
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProficiencyTracker.Models;
 
 namespace ProficiencyTracker.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<User>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
         }
         public DbSet<Student> Students { get; set; }
-        public DbSet<User> Users { get; set; }
         public DbSet<StudentProgress> StudentProgress { get; set; }
         public DbSet<GradeLevel> GradeLevels { get; set; }
         public DbSet<ProficiencyItem> ProficiencyItems { get; set; }
         public DbSet<Subject> Subjects { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
